b1cb05e823b6ced6b5f8b615b1f6415ef9069644
TechnologicalJerry <cserajatverma@gmail.com>
2025-11-16 21:23:57 +0530

    Add user registration route
    Introduces a new route for user registration at /api/users, including OpenAPI documentation, request validation, and handler integration.

---
+++ express-server/src/routes/user.routes.ts
@@ -0,0 +1,35 @@
+import { Express } from "express";
+import { createUserHandler } from "../controller/user.controller";
+import validateResource from "../middleware/validateResource";
+import { createUserSchema } from "../schema/user.schema";
+
+export default function registerUserRoutes(app: Express) {
+  /**
+   * @openapi
+   * '/api/users':
+   *  post:
+   *     tags:
+   *     - User
+   *     summary: Register a user
+   *     requestBody:
+   *      required: true
+   *      content:
+   *        application/json:
+   *           schema:
+   *              $ref: '#/components/schemas/CreateUserInput'
+   *     responses:
+   *      200:
+   *        description: Success
+   *        content:
+   *          application/json:
+   *            schema:
+   *              $ref: '#/components/schemas/CreateUserResponse'
+   *      409:
+   *        description: Conflict
+   *      400:
+   *        description: Bad request
+   */
+  app.post("/api/users", validateResource(createUserSchema), createUserHandler);
+}
+
+
