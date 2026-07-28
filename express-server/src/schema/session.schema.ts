db2b9aa534f782ea786de77da4c6f9f12cd6fdcf
Rajat Verma <cserajatverma@gmail.com>
2026-07-29 02:08:48 +0530

    feat: implement Zod validation schemas and configure Pino logger utility

--- express-server/src/schema/session.schema.ts
+++ express-server/src/schema/session.schema.ts
@@ -56,10 +56,10 @@ import { object, string } from "zod";
 export const createSessionSchema = object({
   body: object({
     email: string({
-      required_error: "Email is required",
+      message: "Email is required",
     }),
     password: string({
-      required_error: "Password is required",
+      message: "Password is required",
     }),
   }),
 });
