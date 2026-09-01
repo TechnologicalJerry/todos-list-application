6109ea96474d5bba73d587d472e162967e95a4e0
Rajat Verma <cserajatverma@gmail.com>
2026-09-01 22:49:34 +0530

    feat(nest-server): configure modules and database connection in AppModule

--- nest-server/src/app.module.ts
+++ nest-server/src/app.module.ts
@@ -1,10 +1,31 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import configuration from './config/configuration';
+import { UsersModule } from './modules/users/users.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { TodosModule } from './modules/todos/todos.module';
+import { ProductsModule } from './modules/products/products.module';
+import { HealthModule } from './modules/health/health.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('dbUri'),
+      }),
+    }),
+    HealthModule,
+    UsersModule,
+    AuthModule,
+    TodosModule,
+    ProductsModule,
+  ],
 })
 export class AppModule {}
